eecc320d020943fca6a158a47af0933b9bea23c8
Allef sousa <allefsousa_1@hotmail.com>
2018-09-02 16:33:03 -0300

    funcionando OK

--- app/src/main/java/com/developer/allef/messagedialog/MainActivity.java
+++ app/src/main/java/com/developer/allef/messagedialog/MainActivity.java
@@ -1,13 +1,31 @@
 package com.developer.allef.messagedialog;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.developer.allef.sweetmessage.SweetAlertDialog;
 
 public class MainActivity extends AppCompatActivity {
-
+TextView textView ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.text);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SweetAlertDialog pDialog = new SweetAlertDialog(MainActivity.this, SweetAlertDialog.ERROR_TYPE);
+                pDialog.getProgressHelper().setBarColor(Color.parseColor("#A5DC86"));
+                pDialog.setTitleText("Loading");
+                pDialog.setContentText("Teste");
+                pDialog.setCancelable(false);
+                pDialog.show();
+            }
+        });
+
     }
 }
